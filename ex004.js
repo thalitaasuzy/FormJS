5b817e267d2f4d5e0a4d060061050ae2be151638
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2023-06-01 15:16:05 -0300

    Create ex004.js

---
+++ ex004.js
@@ -0,0 +1,21 @@
+<html>
+    <style> 
+    p2 {color: yellow;} 
+    body {background: rgb(183, 225, 228)} </style>
+     
+   <head>
+       <meta charset="utf-8">
+       <title>Minha  pagina web!</title>
+   </head>
+   <body>
+     
+         <h1 id="centralizado">Minha  pagina web!</h1> 
+        <p id="negrito" class="centralizado italico">OlÃ¡! Meu Nome Ã© MÃ¡rio!</p>
+        <p>Moro em <a href="https://en.wikipedia.org/wiki/Fortaleza">Fortaleza</a>. </p>
+    </body>
+</html>
+
+<script> document.querySelector ("centralizado") = "Minha Nova Página" 
+var h1Element = document.querySelector("h1");
+h1Element.textContent = "Bem-vindo a Minha Página";
+</script>
